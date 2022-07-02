64f0bf5d403f6ec90ce2f6bc3e086b27b84e9346
RDTank <rdtank112@gmail.com>
2022-07-02 19:54:52 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import MusicPlayer from './src/components/MusicPlayer';
+
+const App = () => {
+  return <MusicPlayer />;
+};
+
+export default App;
